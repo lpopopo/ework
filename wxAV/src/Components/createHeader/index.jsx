52ea50fea15678b8f4bbd716f004ee9e47224efa
lpopopo <2403164743@qq.com>
2019-12-17 00:18:56 +0800

    done

--- wxAV/src/Components/createHeader/index.jsx
+++ wxAV/src/Components/createHeader/index.jsx
@@ -1,4 +1,4 @@
-import Taro,{Component} from '@tarojs/taro'
+import {Component} from '@tarojs/taro'
 
 import './index.scss'
 import { View , Image } from '@tarojs/components';
@@ -10,62 +10,10 @@ import { connect } from '@tarojs/redux'
 }))
 
 class CreateHeader extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      height : 500,
-      width: 500,
-      fileUrl:'',
-      }
-      this.createAv = this.createAv.bind(this)
-  }
-  createAv() {
-    const ctx = Taro.createCanvasContext('header' ,this)
-    const that = this
-    const { url, id } = this.props.counter
-    const backUrl = `../../asset/image/${id + 1}.png`
-    const width = this.state.width
-    const height = this.state.height
-    console.log(backUrl)
-    ctx.arc(0, 0, width / 2, 0, 2 * Math.PI)
-    ctx.clip()
-    ctx.drawImage(url, 0, 0, width, height)
-    ctx.save()
-    ctx.restore()
-    ctx.drawImage(backUrl, 0, 0, width, height)
-    ctx.stroke()
-    ctx.draw(false, () => {
-      console.log('test')
-      setTimeout(() => {
-        Taro.canvasToTempFilePath({
-          x: 0,
-          y: 0,
-          width,
-          height,
-          destWidth: width,
-          destHeight: height,
-          canvasId: 'header',
-          success: function (res) {
-            const fileUrl = res.tempFIlePath
-            console.log(fileUrl)
-            Taro.hideLoading()
-          },
-          fail: function () {
-            console.log('faild')
-            Taro.hideLoading()
-          }
-        }, that)
-     
-      }, 1000);
-     })
-}
-
-
   render() { 
     const {url , images , id} = this.props.counter
     return(
       <View className="pageCanves">
-        <canvas id="header" canvasId="header"></canvas>
         <View className="picShow">
           <Image src={url}></Image>
           <View
@@ -73,9 +21,6 @@ class CreateHeader extends Component {
             style={{ backgroundImage: `url(../../..${images[id]})` }}
             onClick=""
           ></View>
-          <View className="downBtn">
-            <View className="btn" onClick={this.createAv}>保存</View>
-          </View>
         </View>
       </View>
     )
--- wxAV/src/pages/index/index.jsx
+++ wxAV/src/pages/index/index.jsx
@@ -21,17 +21,68 @@ import homePic from '../../asset/image/home.png'
 class Index extends Component {
 
     config = {
-    navigationBarTitleText: '首页'
+      navigationBarTitleText: '首页',
+      navigationBarBackgroundColor: "#ea8567",
   }
 
-  componentWillMount(){
+  constructor(props) {
+    super(props);
+    this.state = {
+      height : 100,
+      width: 100,
+      }
+      this.createAv = this.createAv.bind(this)
   }
-  componentDidMount(){
-  }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
+  createAv() {
+    const ctx = Taro.createCanvasContext('header' ,this)
+    const that = this
+    const { url, id } = this.props.counter
+    const backUrl = `../../asset/image/${id + 1}.png`
+    const width = this.state.width
+    const height = this.state.height
+    ctx.arc(width/2 , height/2, width / 2-4, 0,  2* Math.PI)
+    ctx.clip()
+    ctx.drawImage(url, 0, 0, width+2, height  )
+    ctx.save()
+    ctx.restore()
+    ctx.drawImage(backUrl, 0, 0, width, height )
+    ctx.stroke()
+    ctx.draw(false, () => {
+        Taro.canvasToTempFilePath({
+          x: 0,
+          y: 0,
+          width,
+          height,
+          destWidth: width,
+          destHeight: height,
+          canvasId: 'header',
+          success: function (res) {
+            const fileUrl = res.tempFilePath
+            Taro.saveImageToPhotosAlbum({
+              filePath:fileUrl,
+              success:(res)=>{
+              Taro.showToast({
+                title: '图片保存成功',
+                icon: 'success',
+                duration: 2000
+              })
+              },
+              fail:(err)=>{
+                Taro.showToast({
+                  title: '图片保存失败',
+                  icon: 'none',
+                  duration: 2000
+                })
+              }
+            })
+            Taro.hideLoading()
+          },
+          fail: function () {
+            Taro.hideLoading()
+          }
+        }, that)
+     })
+}
 
   indexChange(id){
     this.props.indexChange(id)
@@ -55,6 +106,10 @@ class Index extends Component {
         })}
       </View>
         <drawImg></drawImg>
+        <View className="downBtn">
+            <View className="btn" onClick={this.createAv}>保存</View>
+          </View>
+          <canvas  canvasId="header"></canvas>
       </View>
     )
 
